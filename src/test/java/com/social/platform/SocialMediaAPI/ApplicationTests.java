105f0f9ea303343565c241ae0f887fda5f6a9149
shady0503 <j130040516@um5.ac.ma>
2025-10-29 19:53:32 +0100

    Add basic application test to generate test reports

---
+++ src/test/java/com/social/platform/SocialMediaAPI/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social.platform.SocialMediaAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // This test verifies that the Spring application context loads successfully
+    }
+}
